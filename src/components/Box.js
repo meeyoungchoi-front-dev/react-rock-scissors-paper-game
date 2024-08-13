3e42c81aa8bc6e7c583abe372a5e3899bba1799e
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-13 16:36:15 +0900

    가위바위보 게임 승자 패자 판단하는 로직 개발

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
 const Box = (props) => {
+  let result = '';
   console.log("props: ", props);
+  if ( props.title === "Computer" && props.result !== "draw" && props.result !== "" ) { 
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    result = props.result;
+  }
+
+  if ( props.title === "Computer" && props.title === "draw") {
+     result = "draw";
+  }
   
   return (
     <div className="box">
         <h1>{props.title}</h1>
         <img className="item-img" src={props.item && props.item.img} alt={props.item && props.item.desc}/>
-        <h2>WIN</h2>
+        <h2>{result}</h2>
     </div>
   )
 }
