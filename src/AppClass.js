def453658064d4ca1348737a58f58ead85a33cae
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-14 08:47:21 +0900

    클래스 컴포넌트로 변경

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,28 +1,83 @@
-import React, { Component } from 'react'
-import BoxClass from './BoxClass'
+import React, { Component } from "react";
+import "./App.css";
+import BoxClass from "./BoxClass";
 
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://media.istockphoto.com/photos/stone-pebble-gray-picture-id1288973456?b=1&k=20&m=1288973456&s=170667a&w=0&h=GBGgp4yrZv4ooDBws8yHF24sJ3rkEpObYsBWpVNKFT8=",
+  },
+  scissors: {
+    name: "Scissors",
+    img: "https://www.ikea.com/kr/en/images/products/sy-scissors__0112301_pe263788_s5.jpg?f=s",
+  },
+  paper: {
+    name: "Paper",
+    img: "https://www.collinsdictionary.com/images/full/paper_111691001.jpg",
+  },
+};
 
 export default class AppClass extends Component {
- constructor(props) {
-    super(props)
+  constructor() {
+    super();
     this.state = {
-        counter2: 0,
-        num: 1,
-        value: 0
-    }
- }
+      userSelect: null,
+      computerSelect: null,
+      result: "",
+    };
+  }
 
- increase = () => {
-    this.setState({counter2: this.state.counter2 + 1, value: this.state.value + 1})
- }
+  play = (userChoice) => {
+    let computerChoice = this.randomChoice();
+    this.setState({
+      userSelect: choice[userChoice],
+      computerSelect: computerChoice,
+      result: this.judgement(choice[userChoice], computerChoice),
+    });
+  };
+  randomChoice = () => {
+    let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만들어주는 함수다
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+  judgement = (user, computer) => {
+    
+     // 내가 승리하는 조건
+    // 가위 : 보 , 바위 : 가위, 보 : 바위
+    // 컴퓨터가 승리하는 조건
+    // 보 : 가위, 가위 : 바위, 바위 : 보
+    if (user.name === computer.name) {
+      return "tie";
+    } else if (user.name === "Rock")
+      return computer.name === "Scissors" ? "win" : "lose";
+    else if (user.name === "Scissors")
+      return computer.name === "Paper" ? "win" : "lose";
+    else if (user.name === "Paper")
+      return computer.name === "Rock" ? "win" : "lose";
+  };
 
   render() {
     return (
       <div>
-        <div>state: {this.state.counter2}</div>
-        <button onClick={this.increase}>클릭하세요</button>
-        <BoxClass num={this.state.value}/>
+        <div className="main">
+          <BoxClass
+            title="You"
+            item={this.state.userSelect}
+            result={this.state.result}
+          />
+          <BoxClass
+            title="Computer"
+            item={this.state.computerSelect}
+            result={this.state.result}
+          />
+        </div>
+        <div className="main">
+          <button onClick={() => this.play("scissors")}>가위</button>
+          <button onClick={() => this.play("rock")}>바위</button>
+          <button onClick={() => this.play("paper")}>보</button>
+        </div>
       </div>
-    )
+    );
   }
 }
